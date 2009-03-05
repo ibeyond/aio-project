e00eee8bcd142fd5493dc1e62f21f3b048de2d44
ibeyond@gmail.com <ibeyond@gmail.com@1666a7fc-0923-11de-b8f3-b9fb9eb71b04>
2009-03-05 05:10:33 +0000

    追加Gdata API

--- trunk/AIO/src/blogsearch/models.py
+++ trunk/AIO/src/blogsearch/models.py
@@ -2,4 +2,15 @@
 from django.utils.translation import ugettext_lazy as _
 from google.appengine.ext import db
 
+class BlogEntry(db.Model):
+    """’ŸŽq"""
 
+    entry_title = db.StringProperty(required=True)
+    date = db.DateTimeProperty(auto_now=True)
+    entry_contents = db.TextProperty(required=True)
+    entry_link = db.LinkProperty(required=True)
+    create_time = db.DateTimeProperty()
+    update_time = db.DateTimeProperty()
+    
+    def __unicode__(self):
+        return '%s = %s' % ('blogsearch','result')
--- trunk/AIO/src/home/models.py
+++ trunk/AIO/src/home/models.py
@@ -2,4 +2,17 @@
 from django.utils.translation import ugettext_lazy as _
 from google.appengine.ext import db
 
+   
+class Feed(db.Model):
+    feed_title = db.StringProperty()
+    feed_id = db.StringProperty()
+    feed_html_link = db.LinkProperty()
+    feed_description = db.StringProperty()
+    feed_language = db.StringProperty()
+    feed_copy_right = db.StringProperty()
+    feed_author = db.ReferenceProperty(db.UserProperty)
+    feed_last_update_date = db.DateTimeProperty()
+    feed_category = db.StringProperty()
+    feed_categary_scheme = db.StringProperty()
+    feed
 
