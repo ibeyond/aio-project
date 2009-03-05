5b1062e10da952c6f748be94e9329adc2ba66d3c
ibeyond@gmail.com <ibeyond@gmail.com@1666a7fc-0923-11de-b8f3-b9fb9eb71b04>
2009-03-05 01:41:06 +0000

    初始化。

---
+++ trunk/AIO/src/blogsearch/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.http import HttpResponseRedirect
+from django.utils.translation import ugettext as _
+from ragendja.template import render_to_response
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Hello Blogsearch!')
+
+
